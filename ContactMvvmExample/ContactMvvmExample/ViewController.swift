24f5893fdc7b7dc9be2c948fbf33df29c3948995
Chandresh Maurya <chandresh@aleph-labs.com>
2019-10-03 10:52:40 +0800

    Code Load on GitHub

---
+++ ContactMvvmExample/ContactMvvmExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactMvvmExample
+//
+//  Created by Chandresh on 3/10/19.
+//  Copyright © 2019 Chandresh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
